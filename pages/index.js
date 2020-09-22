8b14983014f77959c91f02b0bf777c3166f00f2b
RoskiDeluge <roberto.delgado@gmail.com>
2020-09-22 11:32:00 -1000

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,33 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="bg-blue-100 py-20 px-10 text-blue-900">
+      <div className="container mx-auto">
+        <h1 className="mb-12 text-5xl font-bold text-center bg-gradient-to-br from-blue-700 to-red-400 text-transparent bg-clip-text">Good Bots</h1>
+        <h2 className="mb-8 text-lg">Kogi hot chicken waistcoat, cornhole actually lumbersexual messenger bag subway tile sustainable helvetica kale chips mixtape plaid. Everyday carry blog lumbersexual, mumblecore tote bag aesthetic ugh gentrify green juice enamel pin seitan.</h2>
+        {/* Sales Copy: Dream/Pain/Fix */}
+        <div className="mb-10 text-2xl relative">
+          <div className="absolute text-3xl top-0 left-o select-none transform -translate-y-4 -translate-x-4 opacity-25">🤖 💙</div>
+          <p>A bot is great way to engage and retain audience.</p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <p>Bespoke locavore kinfolk poutine pabst ramps jean shorts blog. Affogato normcore tbh, stumptown pork belly banh mi try-hard thundercats 8-bit hell of iPhone flexitarian ennui.</p>
+        <div className="my-10 grid grid-cols-1 lg:grid-cols-4 gap-4">
+          <div className=" bg-white border-2 rounded-lg shadow p-4 text-gray-700 text-xl space-x-3"><span className="inline-block py-1 px-3 rounded bg-red-600 text-red-100">Step 1</span><span>Create a bot</span></div>
+          <div className=" bg-white border-2 rounded-lg shadow p-6 text-gray-700 text-xl">Step 2: Connect your bot with Facebook</div>
+          <div className=" bg-white border-2 rounded-lg shadow p-6 text-gray-700 text-xl">Step 3: Create your first flow</div>
+          <div className=" bg-white border-2 rounded-lg shadow p-6 text-gray-700 text-xl">Step 4: Invite your audience into your bot</div>
+        </div>
+        <div className="prose">
+          <h2>This is a new section</h2>
+          <p>XOXO pour-over shaman, squid listicle gastropub lyft typewriter selfies whatever sustainable chillwave. Polaroid farm-to-table kombucha etsy leggings.</p>
+          <p>Kogi hexagon pug palo santo forage post-ironic squid kitsch echo park williamsburg. Beard 8-bit humblebrag YOLO, raw denim mumblecore semiotics whatever copper mug hoodie live-edge quinoa trust fund post-ironic paleo.</p>
+        </div>
+        {/* Features */}
+        {/* Buy Button */}
+        {/* Testimonials */}
+        {/* FAQ's */}
+      </div>
     </div>
   )
 }
